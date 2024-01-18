7ef2e351bbb51b7f3e56c6cc94f0e6bf83a48b76
Melchs40 <samscodingjourney@gmail.com>
2024-01-17 22:29:43 -0600

    Edit current and future forecasts to display correctly

--- src/index.js
+++ src/index.js
@@ -1,96 +1,82 @@
 import './style.css';
 
 const content = document.getElementById('content');
-const currentData = [];
-const todayData = [];
-const tomorrowData = [];
-const nextDayData = [];
-const conditionImage = document.createElement('img');
-conditionImage.src = '';
-const conditionImage1 = document.createElement('img');
-conditionImage1.src = '';
-const conditionImage2 = document.createElement('img');
-conditionImage2.src = '';
-const conditionImage3 = document.createElement('img');
-conditionImage3.src = '';
+let currentWeather = [];
+const currentIcon = document.createElement('img');
+currentIcon.src = '';
+const futureForecasts = [];
+let city = 'Chicago';
+const inputBox = document.createElement('input');
+inputBox.type = 'text';
+inputBox.id = 'box';
+const searchBtn = document.createElement('button');
+searchBtn.innerHTML = 'Search';
+const infoDiv = document.createElement('div');
+const currentInfo = document.createElement('div');
+content.appendChild(inputBox);
+content.appendChild(searchBtn);
+content.appendChild(infoDiv);
 
 async function fetchWeather() {
   try {
     const response = await fetch(
-      'https://api.weatherapi.com/v1/forecast.json?key=a9a835bcf54a452985d185926233012&q=oshkosh&days=3'
+      `https://api.weatherapi.com/v1/forecast.json?key=a9a835bcf54a452985d185926233012&q=${city}&days=3`
     );
     const weatherData = await response.json();
+
     console.log(weatherData);
-    currentData.push(`Location: ${weatherData.location.name}`);
-    currentData.push(`Condition: ${weatherData.current.condition.text}`);
-    conditionImage.src = `https:${weatherData.current.condition.icon}`;
-    currentData.push(`Temperature: ${weatherData.current.temp_f}`);
-    currentData.push(`Feels like: ${weatherData.current.feelslike_f}`);
-    currentData.push(`Wind: ${weatherData.current.wind_mph}`);
-    const currentInfo = document.createElement('div');
-    currentInfo.innerHTML = currentData;
-    content.appendChild(currentInfo);
-    content.appendChild(conditionImage);
+    currentWeather.push(`Location: ${weatherData.location.name}`);
+    currentWeather.push(`Condition: ${weatherData.current.condition.text}`);
+    currentIcon.src = `https:${weatherData.current.condition.icon}`;
+    currentWeather.push(`Temperature: ${weatherData.current.temp_f}`);
+    currentWeather.push(`Feels like: ${weatherData.current.feelslike_f}`);
+    currentWeather.push(`Wind: ${weatherData.current.wind_mph}`);
+    currentInfo.innerHTML = `${currentWeather[0]}, ${currentWeather[1]}, ${
+      currentWeather[2].split('.')[0]
+    }, ${currentWeather[3].split('.')[0]}, ${currentWeather[4].split('.')[0]}`;
+    infoDiv.appendChild(currentInfo);
+    infoDiv.appendChild(currentIcon);
+
     for (let i = 0; i < 3; i++) {
-      if (i == 0) {
-        todayData.push(
-          `Condition: ${weatherData.forecast.forecastday[0].day.condition.text}`
-        );
-        conditionImage1.src = `https:${weatherData.forecast.forecastday[0].day.condition.icon}`;
-        todayData.push(
-          `High Temp: ${weatherData.forecast.forecastday[0].day.maxtemp_f}`
-        );
-        todayData.push(
-          `Low Temp: ${weatherData.forecast.forecastday[0].day.mintemp_f}`
-        );
-        todayData.push(
-          `Max Wind: ${weatherData.forecast.forecastday[0].day.maxwind_mph}`
-        );
-        const todayInfo = document.createElement('div');
-        todayInfo.innerHTML = todayData;
-        content.appendChild(todayInfo);
-        content.appendChild(conditionImage1);
-      } else if (i == 1) {
-        tomorrowData.push(
-          `Condtion: ${weatherData.forecast.forecastday[1].day.condition.text}`
-        );
-        conditionImage2.src = `https:${weatherData.forecast.forecastday[1].day.condition.icon}`;
-        tomorrowData.push(
-          `High Temp: ${weatherData.forecast.forecastday[1].day.maxtemp_f}`
-        );
-        tomorrowData.push(
-          `Low Temp: ${weatherData.forecast.forecastday[1].day.mintemp_f}`
-        );
-        tomorrowData.push(
-          `Max Wind: ${weatherData.forecast.forecastday[1].day.maxwind_mph}`
-        );
-        const tomorowInfo = document.createElement('div');
-        tomorowInfo.innerHTML = tomorrowData;
-        content.appendChild(tomorowInfo);
-        content.appendChild(conditionImage2);
-      } else if (i == 2) {
-        nextDayData.push(
-          `Condition: ${weatherData.forecast.forecastday[2].day.condition.text}`
-        );
-        conditionImage3.src = `https:${weatherData.forecast.forecastday[2].day.condition.icon}`;
-        nextDayData.push(
-          `High Temp: ${weatherData.forecast.forecastday[2].day.maxtemp_f}`
-        );
-        nextDayData.push(
-          `Low Temp: ${weatherData.forecast.forecastday[2].day.mintemp_f}`
-        );
-        nextDayData.push(
-          `Max Wind: ${weatherData.forecast.forecastday[2].day.maxwind_mph}`
-        );
-        const nextDayInfo = document.createElement('div');
-        nextDayInfo.innerHTML = nextDayData;
-        content.appendChild(nextDayInfo);
-        content.appendChild(conditionImage3);
-      }
+      const forecastData = [];
+      const futureIcon = document.createElement('img');
+      futureIcon.src = '';
+      forecastData.push(
+        `Condition: ${weatherData.forecast.forecastday[i].day.condition.text}`
+      );
+      futureIcon.src = `https:${weatherData.forecast.forecastday[i].day.condition.icon}`;
+      forecastData.push(
+        `High Temp: ${weatherData.forecast.forecastday[i].day.maxtemp_f}`
+      );
+      forecastData.push(
+        `Low Temp: ${weatherData.forecast.forecastday[i].day.mintemp_f}`
+      );
+      forecastData.push(
+        `Max Wind: ${weatherData.forecast.forecastday[i].day.maxwind_mph}`
+      );
+
+      const forecastInfo = document.createElement('div');
+      forecastInfo.innerHTML = `${forecastData[0]}, ${
+        forecastData[1].split('.')[0]
+      }, ${forecastData[2].split('.')[0]}, ${forecastData[3].split('.')[0]}`;
+      infoDiv.appendChild(forecastInfo);
+      infoDiv.appendChild(futureIcon);
+      futureForecasts.push(forecastData);
     }
   } catch (error) {
     console.log(error);
+  } finally {
+    inputBox.value = '';
   }
 }
 
 fetchWeather();
+
+searchBtn.addEventListener('click', () => {
+  while (infoDiv.firstChild) {
+    infoDiv.removeChild(infoDiv.firstChild);
+  }
+  city = inputBox.value;
+  currentWeather = [];
+  fetchWeather();
+});
